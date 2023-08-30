9223996746b2d2951052be017d59e9c0ac11086f
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2023-08-30 16:45:21 -0400

    pendiente fetch

--- rmortyapp/src/app/userslist/userslist.page.ts
+++ rmortyapp/src/app/userslist/userslist.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-userslist',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserslistPage implements OnInit {
 
-  constructor() { }
+  characters = []
+
+  constructor(
+
+    private http: HttpClient
+
+  ) { }
 
   ngOnInit() {
+
+    this.http.get<any>('https://rickandmortyapi.com/api/characters')
+    .subscribe(res=>{
+      console.log(res);
+      // caracteres
+      this.characters = res.results;
+    });
+    // console.log('wena')
   }
 
 }
+
+
